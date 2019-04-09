310e8dd93b3dcb5dd3a0011c5958ffe441cfbfcc
zaki <zihali91@gmail.com>
2019-04-09 21:42:06 +0100

    Added some Vue script to Admin page

--- TheSilkRoadStore.UI/wwwroot/js/admin/main.js
+++ TheSilkRoadStore.UI/wwwroot/js/admin/main.js
@@ -1,7 +1,23 @@
 ﻿var app = new Vue({
     el: '#app',
     data: {
-        price: 0
+        price: 0,
+        showPrice: true,
+    },
+
+    methods: {
+        togglePrice: function () {
+            this.showPrice = !this.showPrice;
+        },
+        alert(v) {
+            alert(v);
+        }
+    },
+
+    computed: {
+        formatPrice: function () {
+            return "£" + this.price;
+        }
     }
 
 });
